f929ec4d6e51c9eb8b6f10653ba40a30b7d05c75
TurboTruite <ladouce.matthieu@gmail.com>
2023-06-08 16:07:05 +0200

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <div>
+            <h1>Accueil</h1>
+            <a href="./logement">logement</a>
+        </div>
+
+
+    )
+
+
+}
+
+export default Home
---
+++ src/components/Logement.js
@@ -0,0 +1,5 @@
+function Logement() {
+    return <h1>logement</h1>
+}
+
+export default Logement;
